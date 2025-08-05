238d8c5d995a24e936f02671e2caca3896a49066
Felipe <o_pugnero@gmail.com>
2025-08-04 22:41:57 -0300

    fixed solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,25 +1,11 @@
 import './UserInfo.scss';
 
-export const UserInfo = props => {
-  if (props.user) {
-    const { user } = props;
-
-    if (!user.name || !user.email) return null;
-
-    return (
-      <a className="UserInfo" href={`mailto:${user.email}`}>
-        {user.name}
-      </a>
-    );
-  }
-
-  const { name, mail } = props;
-
-  if (!name || !mail) return null;
+export const UserInfo = ({ user }) => {
+  if (!user || !user.name || !user.email) return null;
 
   return (
-    <a className="UserInfo" href={`mailto:${mail}`}>
-      {name}
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
     </a>
   );
 };
